8f6103e4e85ec9128ae7d605d688c6fc5c9a5592
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-01-31 16:26:51 +0700

    getUpdateList

---
+++ api/ReqTools.php
@@ -0,0 +1,32 @@
+<?php
+$mysqli = new mysqli("127.0.0.1", "seasonvar", "jnfKLBY7ZfJq6WZG", "seasonvar", 3306);
+$mysqli->set_charset("utf8");
+class ReqTools {
+    function reqDb ($sql) {
+        global $mysqli;
+        $res = $mysqli->query($sql);
+        $result = array();
+        if (gettype($res) == 'object') {
+            while ($row = $res->fetch_assoc()) {
+                $result[] = $row;
+            }
+        }
+        return $result;
+    }
+    function reqPostHttp($url,$body) {
+        $postdata = http_build_query($body);
+        $opts = array(
+            'http' =>
+                array(
+                    'header' => "Content-Type: application/x-www-form-urlencoded\r\n".
+                    "Content-Length: ".strlen($postdata)."\r\n",     
+                    'method'  => 'POST',
+                    'content' => $postdata
+                )
+        );
+        $context  = stream_context_create($opts);
+        $data = json_decode(file_get_contents($url, false, $context));
+        return $data;
+    }
+}
+?>
---
+++ api/getUpdateList.php
@@ -0,0 +1,19 @@
+<?php
+require_once('ReqTools.php');
+
+$content = trim(file_get_contents("php://input"));
+$body = json_decode($content);
+
+$offset = 0;
+if ($body->offset) {
+    if (gettype($body->offset) == "integer") {
+        $offset = $body->offset;
+    }
+} 
+
+$reqTools = new ReqTools();
+$sql = "SELECT * FROM `updateList` LIMIT 50 OFFSET $offset";
+$result = $reqTools->reqDb($sql);
+
+echo json_encode($result,JSON_UNESCAPED_UNICODE);
+?>
