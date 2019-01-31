e26c0d46864e254d19379456349d69930f02d11e
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-01-31 15:03:03 +0700

    deamon js

--- db worker/updateListDeamon/deamon.js
+++ db worker/updateListDeamon/deamon.js
@@ -1,5 +1,7 @@
 var childProcess = require('child_process');
 
+childProcess.exec('php updateListDeamon.php')
+
 setInterval(function () {
-    childProcess.exec('php updateListDeamon.php')
-},5000)
+    childProcess.exec('php updateListDeamon.php >> updateListDeamon.logfile')
+},86400000)
