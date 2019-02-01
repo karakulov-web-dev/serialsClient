bff4ba47f0ffe2f296cba2d652859f15621d34b0
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-02-01 14:08:48 +0700

    get_Season.php

--- api/get_Season.php
+++ api/get_Season.php
@@ -3,10 +3,12 @@ $mysqli = new mysqli("127.0.0.1", "seasonvar", "jnfKLBY7ZfJq6WZG", "seasonvar", 
 $mysqli->set_charset("utf8");
 $content = trim(file_get_contents("php://input"));
 $configReq = json_decode($content, true);
+
 get_Season($configReq['id']);
+
 function get_Season($id) {
 
-    if (gettype($id) !== 'integer') {
+    if (gettype($id) != 'integer') {
         return false;
     }
 
