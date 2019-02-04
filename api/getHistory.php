8ec7339f802c888ad72f8f8d5e2c3557d231cac0
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-02-04 11:20:56 +0700

    get favorites and history

---
+++ api/getHistory.php
@@ -0,0 +1,24 @@
+<?php
+require_once('ReqTools.php');
+
+$content = trim(file_get_contents("php://input"));
+$body = json_decode($content);
+
+if (gettype($body->userMac) == 'string') {
+    $userMac = $mysqli->real_escape_string($body->userMac);
+} else {
+    $error = 'Req typeError';
+}
+
+if ($error) {
+    echo "{error:$error}";
+}
+
+if (!$error) {
+    $reqTools = new ReqTools();
+    $sql = "SELECT * FROM `history` WHERE userMac='$userMac'";
+    $result = $reqTools->reqDb($sql);
+    echo json_encode($result,JSON_UNESCAPED_UNICODE);
+}
+
+?>
