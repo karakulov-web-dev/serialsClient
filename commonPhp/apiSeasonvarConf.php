64921e2e2896bc0642298a914c965a0a9e4cc53c
karalulov.web.dev <karalulov.web.dev@gmail.com>
2019-03-27 10:49:02 +0700

    split Client and server

--- commonPhp/apiSeasonvarConf.php
+++
@@ -1,4 +0,0 @@
-<?php
-$apiSeasonvarConf = new stdClass();
-$apiSeasonvarConf->key = "033238e5";
-?>
--- commonPhp/bdConfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-$bdConfig = new stdClass();
-$bdConfig->host = "127.0.0.1";
-$bdConfig->username = "serialsApi";
-$bdConfig->password = "serialsApi";
-$bdConfig->bdName = "serials";
-$bdConfig->port = 3306;
-?>
